5e067ca40133abfcdc53641f75cac95f18bc0580
Aditya Thakker <aditya.thakker@gmail.com>
2020-03-26 13:30:40 -0400

    Started adding initial classes

---
+++ source/Phoenix.CRM.Integration.Entities/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace Phoenix.CRM.Integration.Entities
+{
+    [DataContract]
+    public class Contact
+    {
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+
+        [DataMember]
+        public string Address { get; set; }
+
+        [DataMember]
+        public string City { get; set; }
+
+        [DataMember]
+        public string State { get; set; }
+
+        [DataMember(Name = "zip")]
+        public string ZipCode { get; set; }
+    }
+}
